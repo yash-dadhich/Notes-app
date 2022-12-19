f688c32d2d00987d9309e679aaa1f3071b0d43ae
yash <yash.dadhich@squarebits.com>
2022-12-19 18:27:39 +0530

    new adds

--- NotesApp/app/src/main/java/com/charging/notesapp/view/MainActivity.kt
+++ NotesApp/app/src/main/java/com/charging/notesapp/view/MainActivity.kt
@@ -1,34 +1,34 @@
 package com.charging.notesapp.view
 
-import android.os.Bundle
-import com.charging.notesapp.R
+import android.content.Intent
 import android.content.res.Configuration
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.lifecycle.ViewModelProvider
-import com.charging.notesapp.NoteApplication
-import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.charging.notesapp.adapter.NoteAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.charging.notesapp.viewmodel.NoteViewModel
+import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.charging.notesapp.viewmodel.NoteViewModelFactory
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.charging.notesapp.databinding.ActivityMainBinding
+import com.charging.notesapp.NoteApplication
+import com.charging.notesapp.R
+import com.charging.notesapp.adapter.NoteAdapter
+import com.charging.notesapp.viewmodel.NoteViewModel
+import com.charging.notesapp.viewmodel.NoteViewModelFactory
 import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
     lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
+        val fab:FloatingActionButton = findViewById(R.id.fab)
         val nightModeSwitch: SwitchCompat = findViewById(R.id.customSwitch)
         val chipView:Chip = findViewById(R.id.chipView)
         val recyclerView: RecyclerView = findViewById(R.id.notesRecycler)
@@ -47,13 +47,21 @@ class MainActivity : AppCompatActivity() {
             NoteApplication.isFirstTime = false
         }
 
-        chipView.setOnClickListener {
+        chipView.setOnCheckedChangeListener { _, isChecked ->
             chipView.animate().rotation(360f).setDuration(1000).start()
             Handler(Looper.getMainLooper()).postDelayed({
-                chipView.text = "grid"
-                chipView.setChipIconResource(R.drawable.ic_round_grid_on_24)
-                recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
-            },500)
+                if (isChecked) {
+                    chipView.text = "grid"
+                    chipView.setChipIconResource(R.drawable.ic_round_grid_on_24)
+                    recyclerView.layoutManager =StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+                }
+                else{
+                    chipView.text = "list"
+                    chipView.setChipIconResource(R.drawable.ic_list)
+                    recyclerView.layoutManager =StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
+                }
+
+            }, 500)
         }
 
         nightModeSwitch.setOnCheckedChangeListener { _, isChecked ->
@@ -77,6 +85,11 @@ class MainActivity : AppCompatActivity() {
         noteViewModel.myAllNotes.observe(this) { notes ->
             // update UI
             noteAdapter.setNote(notes)
+        }
+
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity,NoteAddActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ NotesApp/app/src/main/java/com/charging/notesapp/view/NoteAddActivity.kt
@@ -0,0 +1,12 @@
+package com.charging.notesapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.charging.notesapp.R
+
+class NoteAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add)
+    }
+}
